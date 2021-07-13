f675818e940211caf78549ea7248329b36db2c0e
someshj5 <someshj5@gmail.com>
2021-07-13 18:46:43 +0530

    update

--- djangoflix/djangoflix/db/models.py
+++ djangoflix/djangoflix/db/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 
+# Create your models here.
 class PublishStateOptions(models.TextChoices):
     PUBLISH = 'PU','Publish'
     DRAFT = 'DR','Draft'
     # UNLISTED = 'UN','Unlisted'
     # PRIVATE = 'PR','Private'
-# Create your models here.
