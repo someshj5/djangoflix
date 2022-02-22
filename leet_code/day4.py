9611ab9b2af7d4d791b87bdeab4148a3cac4d8c5
someshj5 <someshj5@gmail.com>
2022-02-23 00:19:05 +0530

    longest substr

--- leet_code/day4.py
+++ leet_code/day4.py
@@ -19,9 +19,26 @@ class Solution:
             else:
                 return False
         return False if stack_bracket else True
+
+
+def lengthOfLongestSubstring(s):
+        if len(set(s)) == len(s):                       
+            return len(s)
+        substring = ''
+        maxLen = 1
+        for i in s:
+            if i not in substring:                      
+                substring = substring + i
+                maxLen = max(maxLen, len(substring))    
+            else:                                       
+                substring = substring.split(i)[1] + i   
+        return maxLen
         
 
 if __name__ == "__main__":
     sol = Solution()
     res = sol.isValid('()')
     print(res)
+
+
+
