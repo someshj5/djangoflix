4be30793983da5b2b89e303a8ed8494dff2347b7
someshj5 <someshj5@gmail.com>
2022-02-22 23:57:00 +0530

    valid parenthesis run

--- leet_code/day4.py
+++ leet_code/day4.py
@@ -1,10 +1,16 @@
 
 # valid Parenthesis
 class Solution:
-    def isValid(self, s: str) -> bool:
-        dictionary_brackets = {'}':'{',')':'(', ']':'['}
+    def isValid(self, s):
+        dictionary_brackets = {
+            '}':'{',
+            ')':'(',
+            ']':'['
+             }
         open_bracket = ('{','(','[')
         stack_bracket = []
+        # s = "()"
+        # s = "()[]{}"
         for each_char in s:
             if stack_bracket == [] or each_char in open_bracket:
                 stack_bracket.append(each_char)
@@ -13,3 +19,9 @@ class Solution:
             else:
                 return False
         return False if stack_bracket else True
+        
+
+if __name__ == "__main__":
+    sol = Solution()
+    res = sol.isValid('()')
+    print(res)
