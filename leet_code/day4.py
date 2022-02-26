6727cea20d66f6b2f71a43f0ea132657f1621a7b
someshj5 <someshj5@gmail.com>
2022-02-26 15:11:52 +0530

    substring

--- leet_code/day4.py
+++ leet_code/day4.py
@@ -33,7 +33,20 @@ def lengthOfLongestSubstring(s):
             else:                                       
                 substring = substring.split(i)[1] + i   
         return maxLen
-        
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        hashset = set()
+        l=0
+        res=0
+        for r in range(len(s)):
+            while s[r] in hashset:
+                hashset.remove(s[l])
+                l+=1
+            hashset.add(s[r])
+            res = max(res,r-l+1)
+        return res
 
 if __name__ == "__main__":
     sol = Solution()
